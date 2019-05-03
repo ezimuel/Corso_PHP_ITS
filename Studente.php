e30b95d2bbd7572a5b46ffeb09a6b2b36d91c3a0
Enrico Zimuel <e.zimuel@gmail.com>
2019-05-03 12:37:44 +0200

    Primo esempio OOP, una classe in PHP

---
+++ Studente.php
@@ -0,0 +1,48 @@
+<?
+/**
+ * Classe di esempio
+ * @author Enrico Zimuel (enrico.zimuel@its-ictpiemonte.it)
+ */
+class Studente {
+    public $nome;
+    public $cognome;
+    public $email;
+    public $dataDiNascita;
+    protected $corsi = [];
+
+    public function addCorso(string $corso): void
+    {
+        if (!isset($this->corsi[$corso])) {
+            $this->corsi[$corso] = 0;
+        }
+    }
+
+    public function addVotoEsame(string $corso, int $voto): void
+    {
+        if (isset($this->corsi[$corso])) {
+            $this->corsi[$corso] = $voto;
+        }
+    }
+
+    public function getMediaEsami(): ?float
+    {
+        if (empty($this->corsi)) {
+            return null;
+        }
+        return array_sum($this->corsi) / count($this->corsi);
+    }
+}
+
+$studente = new Studente();
+$studente->nome = 'Mario';
+$studente->cognome = 'Rossi';
+$studente->email = 'mario.rossi@itspiemonte.it';
+$studente->dataDiNascita = '1/1/1995';
+
+$studente->addCorso('Cloud Services');
+$studente->addCorso('PHP Programming');
+
+$studente->addVotoEsame('Cloud Services', 24);
+$studente->addVotoEsame('PHP Programming', 26);
+
+printf("Media voto esami: %.2f\n", $studente->getMediaEsami());
