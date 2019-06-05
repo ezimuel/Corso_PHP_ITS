8b2130ad9e1ff49d122aa88ad872676592f109c0
Matteo Gregoricchio <m.gregoricchio@studenti.local>
2019-06-05 11:55:26 +0200

    random groups exercise completed, functions added for both exercise requests

--- Esercizi Gregoricchio/esercizio.php
+++ Esercizi Gregoricchio/esercizio.php
@@ -6,7 +6,7 @@ $students = ["Bert Lorenzo","Buscema Antonio","Carbonati Davide","Cella Marco","
 "Sacchet Gabriele","Sartori Alessandro","Sinatra Gianpaolo","Soncin Erika","Vellano Marco",
 "Velocci Giuseppe","Schutt Daniele","Lo Presti Ilaria","Morales Yusdel","Mason Alessandro"];
 
-function arraymix(array $students){
+function HolyTrinityGroup(array $students){
 
     $c = count($students);
     for ($s=0; $s<$c && count($students)!=0; $s++){
@@ -17,13 +17,33 @@ function arraymix(array $students){
                 break;
             } else {
             $int =  array_rand($students);
-            $group .= $students[$int] . ", ";
+            ($i != 2) ? $group .= $students[$int] . ", " : $group .= $students[$int] . ".";
             unset($students[$int]);
             }
         }
         echo (var_dump($group));
     }
-
 }
 
-arraymix($students);
+function BinaryGroup(array $students){
+
+    $c = count($students);
+    for ($s=0; $s<$c && count($students)!=0; $s++){
+        $int = 0;
+        $group = "";
+        for($i = 0; $i<2; $i++){
+            if ($students == NULL){
+                break;
+            } else {
+            $int =  array_rand($students);
+            ($i != 1) ? $group .= $students[$int] . ", " : $group .= $students[$int] . ".";
+            unset($students[$int]);
+            }
+        }
+        echo (var_dump($group));
+    }
+}
+
+HolyTrinityGroup($students);
+echo ("\r\n");
+BinaryGroup($students);
